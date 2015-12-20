be693671950460395ade6ff14f95541b82bc0feb
Christian Schrebe <christian@schrebe.de>
2015-12-20 15:56:51 +0100

    init hapi frame

---
+++ manifest.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const Confidence = require('confidence');
+const Config = require('./config');
+
+
+const criteria = {
+    env: process.env.NODE_ENV
+};
+
+
+const manifest = {
+    $meta: 'This file defines the plot device.',
+    server: {
+        debug: {
+            request: ['error']
+        },
+        connections: {
+            routes: {
+                security: true
+            }
+        }
+    },
+    connections: [{
+        port: Config.get('/port/web'),
+        labels: ['web']
+    }],
+    plugins: {
+        'hapi-auth-basic': {},
+        'lout': {},
+        'inert': {},
+        'vision': {},
+        'good' : {
+          reporters: [{
+              reporter: require('good-console'),
+                  events: {
+                      response: '*',
+                      log: '*'
+                  }
+          }]
+        },
+        'visionary': {
+            engines: { jade: 'jade' },
+            path: './server/web'
+        },
+        'hapi-mongo-models': {
+            mongodb: Config.get('/hapiMongoModels/mongodb'),
+            models: {
+                Account: './server/models/account',
+                AdminGroup: './server/models/admin-group',
+                Admin: './server/models/admin',
+                AuthAttempt: './server/models/auth-attempt',
+                Session: './server/models/session',
+                Status: './server/models/status',
+                User: './server/models/user'
+            },
+            autoIndex: Config.get('/hapiMongoModels/autoIndex')
+        },
+        './server/auth': {},
+        './server/mailer': {},
+        './server/api/accounts': [{ routes: { prefix: '/api' } }],
+        './server/api/admin-groups': [{ routes: { prefix: '/api' } }],
+        './server/api/admins': [{ routes: { prefix: '/api' } }],
+        './server/api/auth-attempts': [{ routes: { prefix: '/api' } }],
+        './server/api/contact': [{ routes: { prefix: '/api' } }],
+        './server/api/index': [{ routes: { prefix: '/api' } }],
+        './server/api/login': [{ routes: { prefix: '/api' } }],
+        './server/api/logout': [{ routes: { prefix: '/api' } }],
+        './server/api/sessions': [{ routes: { prefix: '/api' } }],
+        './server/api/signup': [{ routes: { prefix: '/api' } }],
+        './server/api/statuses': [{ routes: { prefix: '/api' } }],
+        './server/api/users': [{ routes: { prefix: '/api' } }],
+        './server/web/index': {}
+    }
+};
+
+
+const store = new Confidence.Store(manifest);
+
+
+exports.get = function (key) {
+
+    return store.get(key, criteria);
+};
+
+
+exports.meta = function (key) {
+
+    return store.meta(key, criteria);
+};
---
+++ server/auth.js
@@ -0,0 +1,107 @@
+'use strict';
+
+const Boom = require('boom');
+const Async = require('async');
+
+
+const internals = {};
+
+
+internals.applyStrategy = function (server, next) {
+
+    const Session = server.plugins['hapi-mongo-models'].Session;
+    const User = server.plugins['hapi-mongo-models'].User;
+
+    server.auth.strategy('simple', 'basic', {
+        validateFunc: function (request, username, password, callback) {
+
+            Async.auto({
+                session: function (done) {
+
+                    Session.findByCredentials(username, password, done);
+                },
+                user: ['session', function (done, results) {
+
+                    if (!results.session) {
+                        return done();
+                    }
+
+                    User.findById(results.session.userId, done);
+                }],
+                roles: ['user', function (done, results) {
+
+                    if (!results.user) {
+                        return done();
+                    }
+
+                    results.user.hydrateRoles(done);
+                }],
+                scope: ['user', function (done, results) {
+
+                    if (!results.user || !results.user.roles) {
+                        return done();
+                    }
+
+                    done(null, Object.keys(results.user.roles));
+                }]
+            }, (err, results) => {
+
+                if (err) {
+                    return callback(err);
+                }
+
+                if (!results.session) {
+                    return callback(null, false);
+                }
+
+                callback(null, Boolean(results.user), results);
+            });
+        }
+    });
+
+
+    next();
+};
+
+
+internals.preware = {
+    ensureAdminGroup: function (groups) {
+
+        return {
+            assign: 'ensureAdminGroup',
+            method: function (request, reply) {
+
+                if (Object.prototype.toString.call(groups) !== '[object Array]') {
+                    groups = [groups];
+                }
+
+                const groupFound = groups.some((group) => {
+
+                    return request.auth.credentials.roles.admin.isMemberOf(group);
+                });
+
+                if (!groupFound) {
+                    return reply(Boom.notFound('Permission denied to this resource.'));
+                }
+
+                reply();
+            }
+        };
+    }
+};
+
+
+exports.register = function (server, options, next) {
+
+    server.dependency('hapi-mongo-models', internals.applyStrategy);
+
+    next();
+};
+
+
+exports.preware = internals.preware;
+
+
+exports.register.attributes = {
+    name: 'auth'
+};
