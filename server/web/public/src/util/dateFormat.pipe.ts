f0ac6add202a9a08499f55360f98c65d6f0863aa
SeriousShit <hagen@serious-shit.de>
2016-05-03 16:13:00 +0200

    dateformat

--- server/web/public/src/util/dateFormat.pipe.ts
+++ server/web/public/src/util/dateFormat.pipe.ts
@@ -14,17 +14,16 @@ export class FormatedDateFromStringPipe implements PipeTransform {
 
     transform(dateString: string, args: string[]) : string {
         console.log("formatedDateFromString :" + dateString);
-        var document;
+        
         if (dateString === null || dateString === ""  || dateString === undefined) {
-            document = '';
+            return '';
         } else {
             var date = new Date(dateString);
             var options = {
                 year: "numeric", month: "long",
                 day: "numeric"
             };
-            document = date.toLocaleDateString('de-de', options);
+            return date.toLocaleDateString('de-de', options);
         }
-        return document;
     }
 }
